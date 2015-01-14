718ae5ae6628d808f3220a5d9ded779c36957248
Gustavo Almansa Rodrigues <gustavo.almansa@gmail.com>
2015-01-14 18:23:38 +0000

    Chapter 4 - completed

---
+++ rango/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Rango says hey there world!' +
+                        '<br> <a href="/rango/about">About</a>')
+
+
+def about(request):
+    return HttpResponse('This tutorial has been put together by Gustavo Almansa Rodrigues, 2161669' +
+                        '<br><a href="/rango/">Index</a>')
